3b8ebda1ac225d2809cb9538489156f1636dbcce
CindyRodrigues <cindyrodrigues1999@gmail.com>
2025-01-09 12:54:14 +0530

    feat: add teachers details to school view

--- src/features/school/SchoolView.jsx
+++ src/features/school/SchoolView.jsx
@@ -3,14 +3,17 @@ import { useDispatch, useSelector } from "react-redux"
 import Header from "../../components/Header"
 import { updateSchoolStats, setTopStudent } from "./schoolSlice"
 import { fetchStudents } from "../students/studentsSlice"
+import { fetchTeachers } from "../teachers/teachersSlice"
 
 const SchoolView = () => {
     const dispatch = useDispatch()
     const { students } = useSelector((state) => state.students)
+    const { teachers } = useSelector((state) => state.teachers)
     const { schoolStatistics, topPerformingStudent } = useSelector((state) => state.school)
 
     useEffect(() => {
         dispatch(fetchStudents())
+        dispatch(fetchTeachers())
     }, [dispatch])
 
     useEffect(() => {
@@ -18,9 +21,10 @@ const SchoolView = () => {
         const averageAttendance = totalStudents > 0 ? students.reduce((sum, student) => sum + (student.attendance || 0), 0) / totalStudents : 0
         const averageMarks = totalStudents > 0 ? students.reduce((sum, student) => sum + (student.marks || 0), 0) / totalStudents : 0
         const topStudent = totalStudents > 0 ? students.reduce((topPerformingStudent, student) => (student.marks || 0) > (topPerformingStudent?.marks || 0) ? student : topPerformingStudent , {}) : {}
-        dispatch(updateSchoolStats({ totalStudents, averageAttendance, averageMarks, topStudent }))
+        const totalTeachers = teachers.length
+        dispatch(updateSchoolStats({ totalStudents, averageAttendance, averageMarks, topStudent, totalTeachers }))
         dispatch(setTopStudent(topStudent))
-    }, [students, dispatch])
+    }, [students, teachers, dispatch])
 
     return (
         <div>
@@ -31,6 +35,7 @@ const SchoolView = () => {
                 <p>Average Attendance: {schoolStatistics.averageAttendance?.toFixed(2) || 0}</p>
                 <p>Average Marks: {schoolStatistics.averageMarks?.toFixed(2) || 0}</p>
                 <p>Top Student: {topPerformingStudent?.name || "-"}</p>
+                <p>Total Teachers: {schoolStatistics.totalTeachers}</p>
             </main>
         </div>
     )
